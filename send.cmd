17246cd6e30b1a6c9cf2a7a5c116e2d87e6067b4
Jeremy Yen <jeremy5189@gmail.com>
2016-11-10 15:08:12 +0800

    Working with text

--- send.cmd
+++ send.cmd
@@ -1,3 +1,5 @@
-5.0  drawtext reinit 'text=foo';
-10.0 drawtext reinit 'text=bar';
-20.0 drawtext reinit 'text=';
+10.0 drawtext reinit 'text=10 sec';
+20.0 drawtext reinit 'text=20 sec';
+30.0 drawtext reinit 'text=30 sec';
+40.0 drawtext reinit 'text=40 sec';
+50.0 drawtext reinit 'text=50 sec';
--- stream-video-with-text.sh
+++ stream-video-with-text.sh
@@ -2,7 +2,10 @@
 
 # ffmpeg -i test-out.mp4 -vf "sendcmd=f=send.cmd,drawtext=fontsize=20:fontcolor=white:x=100:y=100:text='Hello World'" -acodec copy test-out-text.mp4
 
-ffmpeg -i test-out.mp4 \
+echo "$1"
+
+ffmpeg -re \
+	   -i test-out.mp4 \
 	   -vf "sendcmd=f=send.cmd,drawtext=\
 				fontsize=50:\
 				fontcolor=white:\
@@ -10,4 +13,6 @@ ffmpeg -i test-out.mp4 \
 				x=100:\
 				y=100:\
 				text='Init text, not changed'" \
-	   -acodec copy test-out-text.mp4
+	   -f flv \
+	    -c:v h264 \
+		-c:a aac -strict -2 "$1"
