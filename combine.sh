e572385928e0a02dc8448c38a4340602b6c2c2d1
Jeremy Yen <jeremy5189@gmail.com>
2016-11-11 20:23:38 +0800

    working

--- combine.sh
+++ combine.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 # Length t = 60
-ffmpeg -re -t "$1" -loop 1 -i test-image.png -i test-audio.wav -c:v h264 -c:a aac -strict -2 -b:a 192k -f flv "$2"
+ffmpeg -re \ 
+	   -framerate 1/10 \
+	   -t "$1" \
+	   -loop 1 \
+	   -r 25 \
+	   -i test/img-%03d.png \
+	   -i test-audio.wav \
+	   -pix_fmt yuv420p \
+	   -c:v h264 \
+	   -c:a aac \
+	   -strict -2 \
+	   -b:a 192k \
+	   -f flv "$2"
 
 #ffmpeg -re -i test-out.mp4 -loop 1 -f flv -c:v h264 -c:a aac -strict -2 "$1"
---
+++ overlay-text.sh
@@ -0,0 +1,1 @@
+convert test-image.png -fill '#FFFFFF' -gravity North -pointsize 30 -annotate +0+100 "Make America Great Again $1" $2
