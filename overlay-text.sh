ed200d230c9d10e5b956a124f392743f101df104
Jeremy Yen <jeremy5189@gmail.com>
2016-11-11 20:33:50 +0800

    Change command

--- overlay-text.sh
+++ overlay-text.sh
@@ -1,3 +1,4 @@
-for i in {001..100}; do
-	convert test-image.png -fill '#FFFFFF' -gravity North -pointsize 30 -annotate +0+100 "Make America Great Again $i" test/$i
+for i in {010..100}; do
+	sleep 10
+	convert test-image.png -fill '#FFFFFF' -gravity North -pointsize 30 -annotate +0+100 "Make America Great Again $i" test/img-$i.png
 done
