da0e83bead13db56687d74549f3b8f7094b1093c
Jeremy Yen <jeremy5189@gmail.com>
2016-11-11 20:56:54 +0800

    Upload file

--- combine.sh
+++ combine.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Length t = 60
-ffmpeg -re \
-	-framerate "1/10" \
+ffmpeg \
+	-thread_queue_size 1024 \
+	-framerate 30 \
 	-t "$1" \
 	-loop 1 \
-	-r 25 \
 	-i test/img-%03d.png \
 	-i test-audio.wav \
 	-pix_fmt yuv420p \
@@ -13,6 +13,6 @@ ffmpeg -re \
 	-c:a aac \
 	-strict -2 \
 	-b:a 192k \
-	-f flv "$2"
+	-f mp4 "$2"
 
 #ffmpeg -re -i test-out.mp4 -loop 1 -f flv -c:v h264 -c:a aac -strict -2 "$1"
--- overlay-text.sh
+++ overlay-text.sh
@@ -1,4 +1,3 @@
-for i in {010..100}; do
-	sleep 10
+for i in {001..300}; do
 	convert test-image.png -fill '#FFFFFF' -gravity North -pointsize 30 -annotate +0+100 "Make America Great Again $i" test/img-$i.png
 done
