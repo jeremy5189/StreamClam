2c2c0ea301bced2b6debb6a67e99cb244b9ed748
Denny Huang <denny0223@gmail.com>
2016-11-11 20:27:19 +0800

    Gen image

--- overlay-text.sh
+++ overlay-text.sh
@@ -1,1 +1,3 @@
-convert test-image.png -fill '#FFFFFF' -gravity North -pointsize 30 -annotate +0+100 "Make America Great Again $1" $2
+for i in {001..100}; do
+	convert test-image.png -fill '#FFFFFF' -gravity North -pointsize 30 -annotate +0+100 "Make America Great Again $i" test/$i
+done
