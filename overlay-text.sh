7592bdd39bc51e811e1fc17c6b1ea8e61690947f
Jeremy Yen <jeremy5189@gmail.com>
2016-11-11 21:44:24 +0800

    Add new line in overlay

--- overlay-text.sh
+++ overlay-text.sh
@@ -1,3 +1,9 @@
 for i in {001..300}; do
-	convert test-image.png -fill '#FFFFFF' -gravity North -pointsize 30 -annotate +0+100 "Make America Great Again $i" test/img-$i.png
+	convert \
+	test-image.png \
+	-fill '#FFFFFF' \
+	-gravity North \
+	-pointsize 30 \
+	-annotate +0+100 "Make America Great Again $i" \
+	test/img-$i.png
 done
